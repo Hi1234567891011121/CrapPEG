151fb67d4cdfaf6f4848bfcf488080520dcb833b
NCP3.0 <68864634+Hi1234567891011121@users.noreply.github.com>
2026-07-13 13:42:45 -0400

    Minor Changes

--- Main.cs
+++ Main.cs
@@ -3,7 +3,7 @@ using Raylib_cs;
 namespace CPEG {
     public class Main {
 
-        public static BitReader reader = new("example_02.mpg");
+        public static BitReader reader = new("example_03.mpg");
         public CrapPEG crap = new CrapPEG(reader);
 
         public int screenArea;
@@ -11,12 +11,18 @@ namespace CPEG {
         public Main() {
 
             Raylib.InitWindow(crap.width, crap.height, "Crap");
-            Raylib.SetConfigFlags(ConfigFlags.VSyncHint);
+            
             while (!Raylib.WindowShouldClose())
             {
                 crap.NextFrame();
                 Draw();
+                Input();
             }   
+        }
+
+        public void Input() {
+            if(Raylib.IsKeyPressed(KeyboardKey.P))
+                crap.Pause();
         }
 
         public void Draw()
@@ -28,7 +34,7 @@ namespace CPEG {
             // colours are just stored as RGB, the FrameBuffer is the values of the current frame, the Decoder will continusly update this frame, so just draw from it
             // the framerate update is in the decoder, it should be fine, because we just listening and drawing the colours at the current time, it shouldn't have any sync issues?
               
-            for (int i = 0; i < crap.currentRGBA.Length; ++i) {
+            for (int i = 0; i < crap.screenArea; ++i) {
                 int y = i / crap.width;
                 int x = i % crap.width;
 
